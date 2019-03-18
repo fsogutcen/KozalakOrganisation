08afc1466f459316fffa5c3630b73cbdc4d08005
fsogutcen <fsogutcen@gmail.com>
2019-03-18 11:21:18 +0300

    Kozalak
    My repository web site design

---
+++ WebSite/Assets/js/OrganizatonScript.js
@@ -0,0 +1,14 @@
+﻿//var eleman = true;
+//function TiklaGoster() {
+//    var divYakala = document.getElementById('adres');
+
+//    if (eleman) {
+//        //divYakala.style.visibility = 'visible';
+//        divYakala.style.display = 'inline';
+//    }
+//    else {
+//        //divYakala.style.visibility = 'hidden';
+//        divYakala.style.display = 'none';
+//    }
+//    eleman = !eleman;
+//}
